e67e4a821701d2f4ab15cb3b6afc1acaed152004
Michael Phillips <michael@cpsplumbing.net>
2022-03-17 19:20:05 -0400

    Replaced broken font awesome with manual svg

--- src/lib/modal.svelte
+++ src/lib/modal.svelte
@@ -1,7 +1,5 @@
 <script>
 	import { showModal, modalData } from '../store.js';
-	import Fa from 'svelte-fa/src/fa.svelte';
-	import { faXmark } from '@fortawesome/free-solid-svg-icons';
 	import { fade, scale } from 'svelte/transition';
 </script>
 
@@ -11,7 +9,13 @@
 	on:click|self={() => showModal.set(false)}
 >
 	<div class="modal" transition:scale={{ duration: 200 }}>
-		<div class="cancel" on:click={() => showModal.set(false)}><Fa icon={faXmark} /></div>
+		<div class="cancel" on:click={() => showModal.set(false)}>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+				><path
+					d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
+				/></svg
+			>
+		</div>
 		<div class="header">{$modalData.header}</div>
 		<div class="message">{$modalData.message}</div>
 	</div>
@@ -64,5 +68,6 @@
 		right: 1rem;
 		font-size: 1.6rem;
 		cursor: pointer;
+		width: 2rem;
 	}
 </style>
