1f05175cc47a42f8b7f8f17b9bd296fee68d95f2
Michael Chappel <michael.e.chappel@gmail.com>
2022-03-19 18:47:38 -0400

    rethemed board, color picker, and title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,26 @@
 	import Board from '../lib/board.svelte';
 </script>
 
+<div class="main">
+<h1> Svelte Word</h1>
 <Board />
+</div>
 
 <svelte:head>
 	<style>
 		@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap');
+		@import url('https://fonts.googleapis.com/css2?family=Pavanam&display=swap');
+		@import url('https://fonts.googleapis.com/css2?family=Passion+One&display=swap');
 		body {
 			font-family: 'Open Sans', sans-serif;
 		}
+		main {
+			display: flex;
+			align-items: center;
+			flex-direction: column;
+		}
+		
+
 	</style>
 </svelte:head>
+
