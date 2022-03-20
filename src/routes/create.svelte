d9c6bd4f8a41f5122e93d040f44cf8db4416fef7
Michael Phillips <michael@cpsplumbing.net>
2022-03-19 21:33:29 -0400

    Starting board creation page

---
+++ src/routes/create.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { getBoard } from '../utils/boardGenerator.js';
+	let boards = [];
+	const createNewBoard = () => {
+		// const { wordArray, answers } = getBoard();
+		boards.push({});
+	};
+</script>
+
+<div>Hello world</div>
+
+<button on:click={() => createNewBoard()}>Create</button>
