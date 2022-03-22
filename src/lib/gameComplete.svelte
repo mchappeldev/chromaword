90fec5ec0c035e4ff362f6c864235aa47e30858b
Michael Chappel <michael.e.chappel@gmail.com>
2022-03-22 01:45:57 -0400

    updated gameComplete styles

--- src/lib/gameComplete.svelte
+++ src/lib/gameComplete.svelte
@@ -24,6 +24,41 @@
 <button on:click={saveBoard}>Save Board</button>
 
 <style>
+	button {
+		font-family: 'Open Sans';
+		font-style: normal;
+		font-weight: 600;
+		font-size: 24px;
+		line-height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 20px;
+		border: none;
+		text-align: center;
+		letter-spacing: 0.245em;
+		text-transform: uppercase;
+		margin-top: 40px;
+		cursor: pointer;
+		color: #ffffff;
+
+		height: 40px;
+		left: 120px;
+		top: 549px;
+		margin: 20px;
+
+		background: linear-gradient(
+			90deg,
+			#e57878 0%,
+			#e59978 21.35%,
+			#e5da78 40.63%,
+			#6fcf96 61.46%,
+			#78b7e5 80.73%,
+			#bf7dd6 100%
+		);
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		border-radius: 5px;
+	}
 	.header {
 		font-family: 'Open Sans';
 		font-weight: 600;
