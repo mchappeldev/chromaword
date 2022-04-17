7feda511c6eb2e6251039adf49210ecee91e556f
Michael Phillips <wholestackdev@gmail.com>
2022-04-16 23:05:20 -0400

    Testing if deloyments log you out

--- src/lib/instructions.svelte
+++ src/lib/instructions.svelte
@@ -1,4 +1,5 @@
 <script>
+	console.log('testing if deployments log you out');
 	export let visible = true;
 	import { colors, seenInstructions } from '../store';
 	import { onDestroy } from 'svelte';
